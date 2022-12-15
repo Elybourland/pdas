28f70136f81ab9f04d4139b5fca7ebc7efb0aa84
Elybourland <JEBourland@yahoo.com>
2022-12-15 14:16:17 -0800

    creating core components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ProfileCard from './ProfileCard';
+
+const App = () => {
+  return (
+    <div>
+      <div>Personal Digital Assistants</div>
+
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
+    </div>
+  )
+}
+
+export default App;
